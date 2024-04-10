3113a53c7839d4eb017a2852292e7bc443eda385
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-04-10 10:21:27 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,5 @@ from tkinter import Tk
 from app.main_menu.main import MainMenu
 
 if __name__ == "__main__":
-    root = Tk()
-    main_menu = MainMenu(root)
+    main_menu = MainMenu()
     main_menu.start()
