246b04dde4dbd1ce4621f3af34e1b1da6af640aa
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-04-10 10:21:20 +0200

    Update main.py

--- app/main_menu/main.py
+++ app/main_menu/main.py
@@ -1,9 +1,10 @@
 from tkinter import Tk, ttk
+
 from app.main_menu.services import calculate_root_geometry
+from app.calculator.main import start_calculator
 
 
-class MainMenu:
-    title = 'Personal Helper'
+class MainMenu(Tk):
     widgets_settings = {
         "welcome_label": {
             "font": ("Georgia", 22),
@@ -28,12 +29,13 @@ class MainMenu:
         },
     }
 
-    def __init__(self, root: Tk):
-        self.root = root
-        self.root.title(MainMenu.title)
-        self.root.geometry(calculate_root_geometry(self.root))
-        self.root.resizable(False, False)
+    def __init__(self):
+        super().__init__()
+        self.title("Personal Helper")
+        self.geometry(calculate_root_geometry(self))
+        self.resizable(False, False)
         self.create_widgets()
+        self.make_binds()
 
     def create_widgets(self):
         # Make styles and settings for menu buttons
@@ -42,30 +44,28 @@ class MainMenu:
 
         # Settings for welcome label
         welcome_label_config = MainMenu.widgets_settings['welcome_label']
-        welcome_label = ttk.Label(self.root, text=welcome_label_config['text'], font=welcome_label_config['font'])
+        welcome_label = ttk.Label(self, text=welcome_label_config['text'], font=welcome_label_config['font'])
         welcome_label.pack(anchor=welcome_label_config['anchor'], pady=welcome_label_config["pady"])
 
         # Settings for "Calculator" button
         calculator_button_config = MainMenu.widgets_settings["calculator_button"]
-        calculator_button = ttk.Button(self.root, text=calculator_button_config["text"], style="Menu.TButton")
+        calculator_button = ttk.Button(self, text=calculator_button_config["text"], style="Menu.TButton", command=start_calculator)
         calculator_button.pack(pady=MainMenu.widgets_settings["menu_button"]["pady"])
 
         # Settings for "Weather" button
         weather_button_config = MainMenu.widgets_settings["weather_button"]
-        weather_button = ttk.Button(self.root, text=weather_button_config["text"], style="Menu.TButton")
+        weather_button = ttk.Button(self, text=weather_button_config["text"], style="Menu.TButton")
         weather_button.pack(pady=MainMenu.widgets_settings["menu_button"]["pady"])
 
         # Settings for "Exit" button
         exit_button_config = MainMenu.widgets_settings["exit_button"]
-        exit_button = ttk.Button(self.root, text=exit_button_config["text"], style="Menu.TButton")
+        exit_button = ttk.Button(self, text=exit_button_config["text"], style="Menu.TButton", command=self.destroy)
         exit_button.pack(pady=MainMenu.widgets_settings["exit_button"]["pady"],
                          side=MainMenu.widgets_settings["exit_button"]["side"])
 
+    def make_binds(self):
+        # Bind exit on Esc
+        self.bind("<Escape>", lambda event: self.destroy())
+
     def start(self):
-        self.root.mainloop()
-
-
-if __name__ == "__main__":
-    root = Tk()
-    main_menu = MainMenu(root)
-    main_menu.start()
+        self.mainloop()
