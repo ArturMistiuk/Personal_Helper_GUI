2dfe2f32e3f449a0f2967a7a008ad7defa789e56
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-04-11 14:20:48 +0200

    Update weather_api.py

--- app/weather/weather_api.py
+++ app/weather/weather_api.py
@@ -2,34 +2,20 @@ import requests
 
 
 class WeatherAPI:
-    greeting = "Welcome to the Weather Forecaster!"
     api_key = ""
-    cities = ["Stockholm", "Zaporizhzhia", "Warsaw"]
-    report = {}
+    city = "Stockholm"
 
-    def get_weather_report(self):
-        for city in WeatherAPI.cities:
-            url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WeatherAPI.api_key}&units=metric"
-            try:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    weather_data = response.json()
-                    weather_description = weather_data["weather"][0]["description"]
-                    temperature = weather_data["main"]["temp"]
-                    humidity = weather_data["main"]["humidity"]
-                    wind_speed = weather_data["wind"]["speed"]
-                    WeatherAPI.report[city] = (
-                        f"Description: {weather_description}\n"
-                        f"Temperature: {temperature}°C\n"
-                        f"Humidity: {humidity}%\n"
-                        f"Wind Speed: {wind_speed} m/s"
-                    )
-                else:
-                    return f"Error: {response.status_code} - {response.text}"
-            except Exception as e:
-                return f"Error: {str(e)}"
-
-        return WeatherAPI.report
+    def get_weather_forecast(self):
+        url = f"http://api.openweathermap.org/data/2.5/forecast?q={WeatherAPI.city}&appid={WeatherAPI.api_key}&units=metric"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                forecast_data = response.json()
+                return forecast_data
+            else:
+                return f"Error: {response.status_code} - {response.text}"
+        except Exception as e:
+            return f"Error: {str(e)}"
 
     @staticmethod
     def create_weather_api():
